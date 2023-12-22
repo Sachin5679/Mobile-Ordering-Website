31a5ee86cf3716d4d8c5afd5324509f8f3a3bf00
Sachin Jayadev <sachinjayadev18@gmail.com>
2023-12-22 21:46:02 +0530

    implemented filter functionality

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faFilter } from '@fortawesome/free-solid-svg-icons';
 import MobileCard from './MobileCard';
 import iphone from '../assets/images/iphone.jpeg';
 import samsungs22 from '../assets/images/samsungs22.jpeg';
@@ -12,11 +11,18 @@ const Products = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState([]);
 
+  const [filters, setFilters] = useState({
+    price: '',
+    os: [],
+    processor: [],
+  });
+  const [showFilterOptions, setShowFilterOptions] = useState(false);
+
   const phones = [
-    { id: 1, image: iphone, title: "iPhone 14", price: "10000", os: "iOS", processor: 'Hexa-core', memory: '128GB 6GB RAM', brand: 'Apple' },
-    { id: 2, image: samsungs22, title: "Galaxy S22", price: "36,999", os: "Android", processor: 'Octa-core', memory: '128 GB 8 GB RAM', brand: 'Samsung' },
-    { id: 3, image: huawei, title: "Huawei P30", price: "59,990", os: 'Android', processor: 'Octa-core', memory: '128 GB 6 GB RAM', brand: 'Huawei' },
-    { id: 4, image: oppo, title: "Oppo A94", price: "23,990", os: 'Android', processor: 'Octa-core', memory: '128GB 8GB RAM', brand: 'Oppo' },
+    { id: 1, image: iphone, title: "iPhone 14", price: 69900, os: "iOS", processor: 'Hexa-core', memory: '128GB 6GB RAM', brand: 'Apple' },
+    { id: 2, image: samsungs22, title: "Galaxy S22", price: 36999, os: "Android", processor: 'Octa-core', memory: '128 GB 8 GB RAM', brand: 'Samsung' },
+    { id: 3, image: huawei, title: "Huawei P30", price: 59990, os: 'Android', processor: 'Octa-core', memory: '128 GB 6 GB RAM', brand: 'Huawei' },
+    { id: 4, image: oppo, title: "Oppo A94", price: 23990, os: 'Android', processor: 'Octa-core', memory: '128GB 8GB RAM', brand: 'Oppo' },
   ];
 
   const handleSearch = () => {
@@ -27,7 +33,48 @@ const Products = () => {
     setSearchResults(results);
   };
 
-  const displayPhones = searchTerm ? searchResults : phones;
+  const handleFilterChange = (filterName, value) => {
+    setFilters({ ...filters, [filterName]: value });
+  };
+
+  const handleFiltersOpen = () => {
+    setShowFilterOptions(!showFilterOptions);
+  };
+
+  const displayPhones = () => {
+    let filteredPhones = phones;
+  
+    // Apply search filter
+    if (searchTerm) {
+      const lowerCaseSearchTerm = searchTerm.toLowerCase();
+      filteredPhones = filteredPhones.filter((phone) =>
+        phone.title.toLowerCase().includes(lowerCaseSearchTerm)
+      );
+    }
+  
+    // Apply price filter
+    if (filters.price) {
+      filteredPhones = filteredPhones.filter(
+        (phone) => phone.price <= parseInt(filters.price)
+      );
+    }
+  
+    // Apply OS filter
+    if (filters.os.length > 0) {
+      filteredPhones = filteredPhones.filter((phone) =>
+        filters.os.includes(phone.os)
+      );
+    }
+  
+    // Apply Processor filter
+    if (filters.processor.length > 0) {
+      filteredPhones = filteredPhones.filter((phone) =>
+        filters.processor.includes(phone.processor)
+      );
+    }
+  
+    return filteredPhones;
+  };
 
   return (
     <div className='p-10'>
@@ -37,20 +84,81 @@ const Products = () => {
           placeholder='Search...'
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-          className='w-4/5 px-3 py-2 border border-gray-300 rounded-full shadow-xl'
+          className='w-3/4 px-3 py-2 border border-gray-300 rounded-full shadow-xl focus:border-blue-500 focus:ring focus:outline-none'
         />
         <button
           onClick={handleSearch}
-          className='px-4 py-2 bg-blue-500 shadow-xl text-white rounded-full flex items-center'
+          className='px-4 py-2 bg-blue-500 shadow-xl text-white rounded-full flex items-center hover:text-blue-500 hover:bg-white transition 3s'
         >
           <FontAwesomeIcon icon={faSearch} className='' />
-          
+        </button>
+        <button
+          onClick={handleFiltersOpen}
+          className='px-4 py-2 bg-white shadow-xl text-blue-500 rounded-full flex items-center hover:text-white hover:bg-blue-500 transition 3s'
+        >
+          <FontAwesomeIcon icon={faFilter} className='' />
         </button>
       </div>
 
+      {showFilterOptions && (
+        <div className="mb-4">
+          <label className="block mb-2">
+            Price Range:
+            <input
+              type="range"
+              min="0"
+              max="100000"
+              step="100"
+              value={filters.price}
+              onChange={(e) => handleFilterChange('price', e.target.value)}
+              className="w-full px-4"
+            />
+            {filters.price && (
+              <span className="text-blue-500">Price: Rs. {filters.price}</span>
+            )}
+          </label>
+
+          <label className="block mb-2">
+            Operating System:
+            <select
+              multiple
+              value={filters.os}
+              onChange={(e) =>
+                handleFilterChange(
+                  'os',
+                  Array.from(e.target.selectedOptions, (option) => option.value)
+                )
+              }
+              className="w-full border p-2 rounded"
+            >
+              <option value="Android">Android</option>
+              <option value="iOS">iOS</option>
+            </select>
+          </label>
+
+          <label className="block mb-2">
+            Processor:
+            <select
+              multiple
+              value={filters.processor}
+              onChange={(e) =>
+                handleFilterChange(
+                  'processor',
+                  Array.from(e.target.selectedOptions, (option) => option.value)
+                )
+              }
+              className="w-full border p-2 rounded"
+            >
+              <option value="Hexa-core">HexaCore</option>
+              <option value="Octa-core">OctaCore</option>
+            </select>
+          </label>
+        </div>
+      )}
+
       <div className='flex justify-center'>
         <div className='grid md:grid-cols-4 grid-cols-2 gap-12'>
-          {displayPhones.map(phone => (
+          {displayPhones().map(phone => (
             <MobileCard
               key={phone.id}
               id={phone.id}
