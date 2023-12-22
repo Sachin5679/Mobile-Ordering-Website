5360ab36b5ec97a863b34d644b0b9f4701a21fd2
Sachin Jayadev <sachinjayadev18@gmail.com>
2023-12-22 18:55:15 +0530

    tailwind changes

--- client/src/components/MobileCard.jsx
+++ client/src/components/MobileCard.jsx
@@ -17,7 +17,7 @@ const MobileCard = ({ id, image, title, price, os, processor, memory, brand }) =
         <h3 className='text-xl font-bold'>{title}</h3>
         <p className='font-light'>Rs. {price}</p>
       </div>
-      <button className='bg-blue-500 rounded-lg p-2 w-full mt-2 text-blue-200 hover:text-blue-500 hover:bg-blue-200 transition 3s' onClick={handleOpenModal}>Learn More</button>
+      <button className='bg-blue-500 shadow-xl rounded-lg p-2 w-full mt-2 text-blue-200 hover:text-blue-500 hover:bg-blue-200 transition 3s' onClick={handleOpenModal}>Learn More</button>
 
       
     </div>
--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -12,22 +12,21 @@ const Modal = () => {
 
   return (
     <div className='fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center'>
-      <div className='bg-white p-8 rounded-lg w-96'>
+      <div className='bg-gray-300 p-8 rounded-lg w-96'>
         <div className='flex justify-between items-center mb-4'>
-          <h1 className='text-2xl font-bold'>Phone Details</h1>
+          <h1 className='text-4xl font-bold'>{title}</h1>
           <button className='font-semibold text-red-500' onClick={closeModal}>
             Close
           </button>
         </div>
         <div className='flex items-center space-x-4'>
-          <img className='rounded-xl p-4 w-32 object-cover' src={image} alt={title} />
+          <img className='rounded-xl p-4 w-32 bg-gray-100 shadow-xl object-cover' src={image} alt={title} />
           <div>
-            <h1 className='text-lg font-semibold mb-2'>Name: {title}</h1>
+            <p className='text-sm mb-2'>Brand: {brand}</p>
             <p className='text-sm mb-2'>Price: Rs. {price}</p>
             <p className='text-sm mb-2'>Operating System: {os}</p>
             <p className='text-sm mb-2'>Processor: {processor}</p>
             <p className='text-sm mb-2'>Memory: {memory}</p>
-            <p className='text-sm mb-2'>Brand: {brand}</p>
           </div>
         </div>
       </div>
